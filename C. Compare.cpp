23a0c167d13a94b1c324b05baa8b73a6e79a3023
Ragdha El-gaidi <76912120+Ragdha-Elgaidi@users.noreply.github.com>
2023-01-15 22:55:25 +0200

    Create C. Compare.cpp

---
+++ C. Compare.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<string>
+#include<algorithm>
+using namespace std;
+
+int main()
+{
+    string S,T;
+
+    cin >> S >> T;
+
+    cout << min(S, T) << endl;
+    
+    return 0;
+}
