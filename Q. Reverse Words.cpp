e0db9df05f87cb84f6103b680881a1ac77253be7
Ragdha El-gaidi <76912120+Ragdha-Elgaidi@users.noreply.github.com>
2023-10-21 20:17:13 +0300

    Update Q. Reverse Words.cpp

--- Q. Reverse Words.cpp
+++ Q. Reverse Words.cpp
@@ -1,1 +1,24 @@
 
+#include <iostream>
+#include <string>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+    string s;
+    int flag = 0;
+    while (cin >> s)
+    {
+        reverse(s.begin(), s.end());
+
+        if (flag)
+        {
+            cout << " ";
+        }
+
+        flag = 1;
+        cout << s;
+    }
+}
