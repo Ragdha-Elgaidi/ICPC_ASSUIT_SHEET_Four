3b7e5987c6fa455e09cd24ba0d57e18598673da1
Ragdha El-gaidi <76912120+Ragdha-Elgaidi@users.noreply.github.com>
2023-10-21 19:47:33 +0300

    Update M. Subsequence String.cpp

--- M. Subsequence String.cpp
+++ M. Subsequence String.cpp
@@ -1,1 +1,24 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
 
+using namespace std;
+
+int main()
+{
+    string s, x="hello";
+    cin >> s;
+
+    int j = 0;
+
+    for (int i = 0; i < s.size(); i++)
+    {
+        if (s[i] == x[j])
+            j++;
+    }
+
+    if (j == 5)
+        cout << "YES" << endl;
+    else
+        cout << "NO" << endl;
+}
