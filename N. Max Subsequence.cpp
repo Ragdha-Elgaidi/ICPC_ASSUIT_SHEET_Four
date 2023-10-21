977131aa7d631656efaa92bcc3c6b67e05d0fce5
Ragdha El-gaidi <76912120+Ragdha-Elgaidi@users.noreply.github.com>
2023-10-21 19:55:50 +0300

    Update N. Max Subsequence.cpp

--- N. Max Subsequence.cpp
+++ N. Max Subsequence.cpp
@@ -1,1 +1,21 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
 
+using namespace std;
+
+int main()
+{
+    int n, counter = 0;
+    string s;
+    cin >> n >> s;;
+
+    for (int i = 0; i < n; i++)
+    {
+        if (s[i] != s[i + 1])
+            counter++;
+    }
+
+    cout << counter << endl;
+    
+}
