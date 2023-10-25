8fed3090be484876006f721336c8a98f44eec71d
Ragdha El-gaidi <76912120+Ragdha-Elgaidi@users.noreply.github.com>
2023-10-26 01:57:19 +0300

    Update O. Sort String.cpp

--- O. Sort String.cpp
+++ O. Sort String.cpp
@@ -1,1 +1,27 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
 
+using namespace std;
+
+int main()
+{
+    int size, arr[26] = { 0 };
+    cin >> size;
+
+    for (int i = 0; i < size; i++)
+    {
+        char c;
+        cin >> c;
+        arr[(int)c - 97]++;
+    }
+
+    for (int i = 0; i < 26; i++)
+    {
+        while (arr[i] != 0)
+        {
+            cout << (char)(i + 97);
+            arr[i]--;
+        }
+    }
+}
