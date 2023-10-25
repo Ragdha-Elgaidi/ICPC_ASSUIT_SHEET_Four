330c9f9f7cff2b3805144c8fe757b7655507d9a3
Ragdha El-gaidi <76912120+Ragdha-Elgaidi@users.noreply.github.com>
2023-10-26 00:30:33 +0300

    Update T. URL.cpp

--- T. URL.cpp
+++ T. URL.cpp
@@ -1,1 +1,35 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
 
+using namespace std;
+
+int main()
+{
+    string url;
+    cin >> url;
+    
+    for (int i = 0; i < url.size(); i++)
+    {
+        if (url[i] == '?')
+        {
+            i++;
+            for (int j = i; j < url.size(); j++)
+            {
+                if (url[j] == '=')
+                {
+                    cout << ": ";
+                    continue;
+                }
+                if (url[j] == '&')
+                {
+                    cout << endl;
+                    continue;
+
+                }
+                cout << url[j];
+               
+            }
+        }
+    }
+}
