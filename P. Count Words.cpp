5b970ad51a386957d6eda94cc9cc61911c002722
Ragdha El-gaidi <76912120+Ragdha-Elgaidi@users.noreply.github.com>
2023-10-26 20:34:12 +0300

    Update P. Count Words.cpp

--- P. Count Words.cpp
+++ P. Count Words.cpp
@@ -1,1 +1,28 @@
 
+#include <iostream>
+#include <string>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+    string s,r;
+    getline(cin,s);
+
+    int counter=0;
+
+    for (int i = 0; i < s.size(); i++)
+    {
+        if (s[i] != ' ' && s[i] != '.' && s[i] != ',' && s[i] != '!' && s[i] != '?')
+        {
+            r += s[i];
+        }
+        if (( s[i] == ' ' || s[i] == '.' || s[i] == ',' || s[i] == '!' || s[i] == '?'|| i == s.size()-1) && r.size() != 0)
+        {
+            counter++;
+            r = "";
+        }
+    }
+    cout << counter;
+}
