80a5dbda955ca19ff263c0deac2bcf324da7ed1a
Ragdha El-gaidi <76912120+Ragdha-Elgaidi@users.noreply.github.com>
2023-10-26 21:09:44 +0300

    Update S. Max Split.cpp

--- S. Max Split.cpp
+++ S. Max Split.cpp
@@ -1,1 +1,29 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
 
+using namespace std;
+
+int main()
+{
+    string s, ans="";
+    cin >> s;
+
+    int r = 0, l = 0, counter=0;
+
+    for (int i = 0; i < s.size(); i++)
+    {
+        ans += s[i];
+        if (s[i] == 'R')
+            r++;
+        if (s[i] == 'L')
+            l++;
+        if(l==r)
+        {
+            counter++;
+            ans += '\n';
+        }
+    }
+
+    cout << counter <<'\n'<<ans;
+}
