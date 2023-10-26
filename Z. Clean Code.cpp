ac040ac2bb151418ef75c420e2a585a29b3e31f8
Ragdha El-gaidi <76912120+Ragdha-Elgaidi@users.noreply.github.com>
2023-10-26 22:21:59 +0300

    Update Z. Clean Code.cpp

--- Z. Clean Code.cpp
+++ Z. Clean Code.cpp
@@ -1,1 +1,50 @@
 
+#include <iostream>
+#include <string>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+	string line;
+	int open = 1;
+
+	while (getline(cin, line))
+	{ 
+		bool flag = 0;
+
+		if (line.size() == 0 || line == " ") 
+		{
+			continue;
+		}
+
+		for (int i = 0; i < line.length(); i++)
+		{
+			if (line[i] == '/' && line[i + 1] == '/' && open) 
+			{
+				break;
+			}
+			else if (line[i] == '/' && line[i + 1] == '*') 
+			{ 
+				i++;
+				open = 0;
+			}
+			else if (line[i] == '*' && line[i + 1] == '/' && !open)
+			{ 
+				i++;
+				open = 1;
+			}
+			else if (open == 1)
+			{
+				cout << line[i];
+				flag = 1;
+			}
+		}
+
+		if (flag && open == 1)
+		{
+			cout << endl;
+		}
+	}
+}
