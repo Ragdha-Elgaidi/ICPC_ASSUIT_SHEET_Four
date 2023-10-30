6c58d49fe06e9d0389fff0e2258516333c26c9ef
Ragdha El-gaidi <76912120+Ragdha-Elgaidi@users.noreply.github.com>
2023-10-30 04:06:11 +0200

    Update W. Encrypt & Decrypt Message.cpp

--- W. Encrypt & Decrypt Message.cpp
+++ W. Encrypt & Decrypt Message.cpp
@@ -1,1 +1,46 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
 
+using namespace std;
+
+int main()
+{
+	string key = "PgEfTYaWGHjDAmxQqFLRpCJBownyUKZXkbvzIdshurMilNSVOtec#@_!=.+-*/", 
+	original = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789",
+		s;
+	int q, z = 0, index=0;
+
+	cin >> q >> s;
+
+	if (q == 1)
+	{
+		for (int i = 0; i < s.size(); i++)
+		{
+			for (int z = 0; z < original.size(); z++)
+			{
+				if (s[i] == original[z])
+				{
+					index = z;
+					break;
+				}
+			}
+			cout << key[index];
+		}
+	}
+	else if (q == 2)
+	{
+		for (int i = 0; i < s.size(); i++)
+		{
+			for (int z = 0; z < key.size(); z++)
+			{
+				if (s[i] == key[z])
+				{
+					index = z;
+					break;
+				}
+			}
+			cout << original[index];
+		}
+	}
+}
