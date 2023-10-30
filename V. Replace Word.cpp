7b70c12f7d0f4f06d8058bf778b38079a9c2612a
Ragdha El-gaidi <76912120+Ragdha-Elgaidi@users.noreply.github.com>
2023-10-30 03:06:54 +0200

    Update V. Replace Word.cpp

--- V. Replace Word.cpp
+++ V. Replace Word.cpp
@@ -1,1 +1,25 @@
 
+#include <iostream>
+#include <string>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+	string s;
+	cin >> s;
+
+	for (int i = 0; i < s.size(); i++)
+	{
+		if (s[i] == 'E' && s[i + 1] == 'G'&& s[i + 2] == 'Y'  && s[i + 3] == 'P' && s[i+4] == 'T')
+		{
+			i += 4;
+			cout << " ";
+		}
+		else
+		{
+			cout << s[i];
+		}
+	}
+}
